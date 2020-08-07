d2586ac7ad68fe49403ef790ad39d3123b64b350
internetcharles <charlesodellsmith@gmail.com>
2020-08-07 14:11:23 -0700

    a little styling

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -7,12 +7,14 @@ export default class SearchBar extends Component {
             <div className="searchbar">
                 <input onChange={this.props.handleChange} placeholder="Pokemon Name"></input>
                 {/* <input onChange={this.props.handleAttackChange} placeholder="Min. Attack"></input> */}
+                <p>
                 <select onChange={this.props.handleSearchType}>
                     <option value='pokemon'>Pokemon</option>
                     <option value='attack'>Min. Attack</option>
                     <option value='defense'>Min. Defense</option>
                     <option value='type_1'>Type</option>
                 </select>
+                </p>
                 <button onClick={this.props.handleClick}>Submit</button>
                 <p className="page-count">Page {this.props.currentPage} / {this.props.maxPages} </p>
                 <p>
